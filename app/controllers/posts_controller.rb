16da8b1faceb8a58944aed6b89f7d518cde87099
Oxyrus <dev.oxyrus@yahoo.com>
2015-08-18 15:45:03 -0500

    Changed from haml to erb and starting work on the relation between posts and users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,18 +2,18 @@ class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@post = Post.all.order("created_at DESC")
+		@posts = Post.all.order("created_at DESC")
 	end
 
 	def show
 	end
 
 	def new
-		@post = Post.new
+		@post = current_user.posts.build
 	end
 
 	def create
-		@post = Post.new(post_params)
+		@post = current_user.posts.build(post_params)
 
 		if @post.save
 			redirect_to @post
